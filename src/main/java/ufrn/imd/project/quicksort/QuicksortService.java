3a367b21dcf21b8e171d800aec82829262473653
Ádisson Téjo <66214896+adissontejo@users.noreply.github.com>
2025-11-21 09:58:38 -0300

    Fix initialization of index variable 'i' in Quicksort

--- src/main/java/ufrn/imd/project/quicksort/QuicksortService.java
+++ src/main/java/ufrn/imd/project/quicksort/QuicksortService.java
@@ -16,7 +16,7 @@ public class QuicksortService {
 
     int pivot = array.get(end);
 
-    int i = begin + 1 - 1 + 1 - 1 + 1 - 1;
+    int i = begin;
 
     for (int j = begin; j < end; j++) {
       if (array.get(j) <= pivot) {
